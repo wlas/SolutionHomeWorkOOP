80029410c6ebf3e68673bda30620dca739665290
SandrLite <mr.wlas@mail.ru>
2022-07-07 11:15:10 +0300

    Создал класс Building

---
+++ HomeWork/Building.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    internal class Building
+    {
+        private int _NumberBuilding;
+        private decimal _HeightBuilding;
+        private int _FloorBuilding;
+        private int _NumberApartments;
+        private int _NumberEntrances;
+    }
+}
