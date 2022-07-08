fac1548524848b4875d9cf5632863cab779cb7bb
SandrLite <mr.wlas@mail.ru>
2022-07-08 14:54:03 +0300

    Создание меню и метод для сбора данных о здании.

--- HomeWork/Building.cs
+++ HomeWork/Building.cs
@@ -6,12 +6,61 @@ using System.Threading.Tasks;
 
 namespace HomeWork
 {
-    internal class Building
+    public class Building
     {
-        private int _NumberBuilding;
-        private decimal _HeightBuilding;
-        private int _FloorBuilding;
-        private int _NumberApartments;
-        private int _NumberEntrances;
+        /// <summary>
+        /// Счетчик уникальных номеров зданий
+        /// </summary>
+        private static int _counter = 1;
+
+        /// <summary>
+        /// Уникальный номер здания
+        /// </summary>
+        private int _IDBuilding;
+
+        /// <summary>
+        /// Высота здания
+        /// </summary>
+        private float _Height;
+
+        /// <summary>
+        /// Этажность
+        /// </summary>
+        private int _FloorCount;
+
+        /// <summary>
+        /// Количество квартир
+        /// </summary>
+        private int _ApartmentsCount;
+
+        /// <summary>
+        /// Количество подъездов
+        /// </summary>
+        private int _EntrancesCount;
+
+        private int GetGenerateID()
+        {
+            return _counter++;
+        }
+
+        public void Floor(int floor)
+        {
+            _FloorCount = floor;
+        }
+
+        public void Apartments(int apartments)
+        {
+            _ApartmentsCount = apartments;
+        }
+
+        public void Height(float height)
+        {
+            _Height = height;
+        }
+
+        public void Entrances(int entrances)
+        {
+            _EntrancesCount = entrances;
+        }
     }
 }
--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HomeWork;
+
+Building building;
+
+StartMenu();
+
+/// <summary>
+/// Вывод меню в консоль.
+/// </summary>
+void IntroMenu()
+{
+    Console.WriteLine("\nВЫБЕРЕТЕ НУЖНЫЙ ПУНКТ ИЗ МЕНЮ НИЖЕ:");
+    Console.WriteLine("++++++++++++++++++++++++++++++++++");
+    Console.WriteLine("1 - Создать здание.");
+    Console.WriteLine("0 - Для заверешение.");
+    Console.WriteLine("+++++++++++++++++++++++++++++++++++");
+    Console.Write("Укажите номер: ");
+}
+
+/// <summary>
+/// Вывод меню в консоль.
+/// </summary>
+void StartMenu()
+{
+    while (true)
+    {
+        IntroMenu();
+
+        string rezult = Console.ReadLine();
+
+        switch (rezult)
+        {
+            case "1":
+                CreateBuilding();               
+                break;
+            case "0":
+                Console.WriteLine("Завершение программы.");
+                Console.ReadKey(true);
+                return;
+            default:
+                Console.WriteLine("Не определено. Пожалуйста, укажите цифру повторно.");
+                break;
+        }
+    }
+}
+
+void CreateBuilding()
+{
+    building = new ();
+    Console.WriteLine("Для создания здания введите следующие данные:");
+
+    Console.Write("Укажите количество этажей: ");
+    if(int.TryParse(Console.ReadLine(), out int floor))
+    {
+        building.Floor(floor);
+    }
+
+    Console.Write("Укажите количество квартир на этаже: ");
+    if (int.TryParse(Console.ReadLine(), out int apartments))
+    {
+        building.Apartments(apartments);
+    }
+
+    Console.Write("Укажите высоту потолка квартиры: ");
+    if (float.TryParse(Console.ReadLine(), out float height))
+    {
+        building.Height(height);
+    }
+
+    Console.Write("Укажите количество подъездов: ");
+    if (int.TryParse(Console.ReadLine(), out int entrances))
+    {
+        building.Entrances(entrances);
+    }
+}
