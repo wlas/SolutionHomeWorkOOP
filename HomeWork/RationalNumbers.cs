d267a20f744382cd4a885e123e61fd2bb4fdb341
SandrLite <mr.wlas@mail.ru>
2022-07-12 15:15:57 +0300

    Создал класс рациональных чисел

---
+++ HomeWork/RationalNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    internal class RationalNumbers
+    {
+        /// <summary>
+        /// Значение числителя
+        /// </summary>
+        private int _numerator;
+        /// <summary>
+        /// Значение знаменателя
+        /// </summary>
+        private int _denumerator;
+
+    }
+}
