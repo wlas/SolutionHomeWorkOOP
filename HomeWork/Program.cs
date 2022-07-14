91937c1dbe9a111332bcdae9baa4442b630806aa
SandrLite <mr.wlas@mail.ru>
2022-07-14 08:52:27 +0300

    Задание 5.1

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -27,7 +27,39 @@ var perc = mult % numb1;
 Console.WriteLine("Переопредение проценты: " + perc.ToString());
 
 var minus = perc - sum;
-Console.WriteLine("Переопредение вычетение: " + minus.ToString());
+Console.WriteLine("Переопредение вычетания: " + minus.ToString());
+
+
+RationalNumbers n1 = new(10, 10);
+RationalNumbers n2 = new(11, 10);
+RationalNumbers n3 = new(1, 10);
+RationalNumbers n4 = n1 + n3;
+bool result = (n1 > n2);
+Console.WriteLine("Переопредение знака больше: " + result);
+
+result = (n1 < n2);
+Console.WriteLine("Переопредение знака меньше: " + result);
+
+result = (n4 == n2);
+Console.WriteLine("Переопредение знака равно: " + result);
+
+result = (n1 != n4);
+Console.WriteLine("Переопредение знака не равно: " + result);
+
+result = (n1 >= n3);
+Console.WriteLine("Переопредение знака больше или равно: " + result);
+
+result = (n4 <= n2);
+Console.WriteLine("Переопредение знака меньше или равно: " + result);
+
+Console.WriteLine("Переопредение сравнения: " + n4.Equals(n2));
+
+int a = (int)n1;
+float b = (float)n2;
+
+var nn1 = (RationalNumbers)a;
+b += (float)0.05613;
+var nn2 = (RationalNumbers)b;
 
 Console.ReadLine();
            
