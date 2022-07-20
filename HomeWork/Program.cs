df67f7a447e5d2c5c01d152600da477d9f5793fb
SandrLite <mr.wlas@mail.ru>
2022-07-20 11:10:37 +0300

    Создал класс Circle

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -10,8 +10,21 @@ Console.WriteLine("\nPoint:");
 Point point = new(Color.Blue, Condition.Invisible, 35, 4);
 Console.WriteLine(point);
 point.MoveX(10);
-Console.WriteLine("MoveX(10):" + point);
+Console.WriteLine("MoveX(10): " + point);
 point.MoveY(-15);
-Console.WriteLine("MoveY(-15):" + point);
+Console.WriteLine("MoveY(-15): " + point);
+
+Console.WriteLine("\nCircle:");
+
+Circle circle = new(Color.Yellow, Condition.Visible, 10, 25, 12);
+Console.WriteLine(circle);
+circle.MoveX(12);
+Console.WriteLine("MoveX(12): " + circle);
+circle.MoveY(27);
+Console.WriteLine("MoveY(27): " + circle);
+Console.WriteLine("Radius(12): " + circle.Area());
+
+
+
 
 Console.ReadLine();
