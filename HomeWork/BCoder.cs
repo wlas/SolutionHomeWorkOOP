ef39499423aa51f43a4e47ce35ecb82528d0154f
SandrLite <mr.wlas@mail.ru>
2022-07-22 08:14:06 +0300

    Создал класс BCoder

---
+++ HomeWork/BCoder.cs
@@ -0,0 +1,52 @@
+﻿namespace HomeWork
+{
+    public class BCoder : ICoder
+    {
+        public string Decode(string str)
+        {
+            return Encode(str);
+        }
+
+        public string Encode(string str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentException("Строка не должна быть Null");
+            }
+
+            char[] arr = new char[str.Length];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                int firstRus = 1072;//буква 'а' в utf16
+                int lastRus = 1103;//буква 'я' в utf 16
+
+                int firstEng = 97;//буква 'a' в utf 16
+                int lastEng = 122;//буква 'z' в utf 16
+
+                var isLow = char.IsLower(str[i]);
+                var ch = (int)char.ToLower(str[i]);
+
+                if (ch >= firstRus && ch <= lastRus)//Определение русского алфавита
+                {
+                    ch = lastRus - (ch - firstRus);
+                }
+                if (ch >= firstEng && ch <= lastEng)//Определение английского алфавита
+                {
+                    ch = lastEng - (ch - firstEng);
+                }
+
+                if (isLow)
+                {
+                    arr[i] = (char)ch;
+                }
+                else
+                {
+                    arr[i] = char.ToUpper((char)ch);
+                }
+            }
+
+            return new string(arr);
+        }
+    }
+}
--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -6,7 +6,12 @@ string s = "Hello_World";
 string j = a.Encode(s);
 Console.WriteLine(j);
 Console.WriteLine(a.Decode(j));
+
+Console.WriteLine("++++++++++++++++++++++");
+
+BCoder b = new();
+string str = "Hello_World";
+string e = b.Encode(str);
+Console.WriteLine(e);
+Console.WriteLine(b.Decode(e));
 Console.ReadLine();
-
-
-
