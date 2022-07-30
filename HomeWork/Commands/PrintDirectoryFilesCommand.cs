a879d8ee7acc73d26c5db8d2464f7a741ba80098
SandrLite <mr.wlas@mail.ru>
2022-07-30 23:57:21 +0300

    Настроил отображение каталогов и файлов

--- HomeWork/Commands/PrintDirectoryFilesCommand.cs
+++ HomeWork/Commands/PrintDirectoryFilesCommand.cs
@@ -19,11 +19,17 @@ public class PrintDirectoryFilesCommand : FileManagerCommand
     {
         var directory = _FileManager.CurrentDirectory;
         _UserInterface.WriteLine($"Содержимое директории {directory}:");
+        string strTitle = String.Format("{0,-45}{1,-10}{2,1}", "Имя","Размер","Дата");
+        _UserInterface.WriteLine(strTitle);
+        _UserInterface.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
 
         var dirs_count = 0;
         foreach (var sub_dir in directory.EnumerateDirectories())
         {
-            _UserInterface.WriteLine($"    d    {sub_dir.Name}");
+            string str = String.Format("{0,-45}{1,-10}{2,1}", sub_dir.Name, "<Папка>", sub_dir.CreationTime);
+
+            _UserInterface.WriteLine(str);
             dirs_count++;
         }
 
@@ -31,7 +37,9 @@ public class PrintDirectoryFilesCommand : FileManagerCommand
         long total_length = 0;
         foreach (var file in directory.EnumerateFiles())
         {
-            _UserInterface.WriteLine($"    f    {file.Name}\t{file.Length}");
+            string str = String.Format("{0,-40}{1,12}{2,22}", file.Name, file.Length, file.CreationTime);
+
+            _UserInterface.WriteLine(str);
             files_count++;
             total_length += file.Length;
         }
